2cac3e8d6c96ea32ad081365ed7b322ece23c95f
Drew Rodman <dcrodman@gmail.com>
2015-04-01 20:36:24 -0700

    Add Homebrew bash installer.

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# Install homebrew if it isn't already installed
+if ! hash brew 2> /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew upgrade
+
+brew install python
+brew install ruby
+brew install wget
+brew install vim
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install spotify
+brew cask install steam
+
+# Remove outdated versions from the Cellar
+brew cleanup
+
+# Link .app files into /Applications
+brew linkapps
