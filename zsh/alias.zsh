7c384c91807336bed207fa3320e6335b74add2ff
Drew Rodman <arodman@pandora.com>
2015-04-02 16:02:27 -0700

    Add hidden file aliases.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,7 @@
 alias setgo='export GOPATH="$(pwd)"'
 
+alias shidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
+alias hhidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
+
 alias startmysql='mysql.server start'
 alias stopmysql='mysql.server stop'
