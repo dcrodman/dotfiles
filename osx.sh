2570fc4ecdb0066ab48d3b7de57ec3728e941c91
Drew Rodman <arodman@pandora.com>
2015-04-07 15:31:54 -0700

    Don't minimize windows to their dock icons.

--- osx.sh
+++ osx.sh
@@ -289,12 +289,6 @@ defaults write com.apple.dock mouse-over-hilite-stack -bool true
 # Set the icon size of Dock items to 36 pixels
 # defaults write com.apple.dock tilesize -int 36
 
-# Change minimize/maximize window effect
-# defaults write com.apple.dock mineffect -string "scale"
-
-# Minimize windows into their application’s icon
-defaults write com.apple.dock minimize-to-application -bool true
-
 # Enable spring loading for all Dock items
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
