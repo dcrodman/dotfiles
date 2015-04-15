60cb038fcdeafef530997ee47fbcbd5ae323e064
Drew Rodman <arodman@pandora.com>
2015-04-15 15:19:46 -0700

    Add /usr/local/sbin to PATH.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,4 @@
 export GOROOT='/usr/local/go'
 
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=${PATH}:/opt/X11/include:/opt/X11/bin
