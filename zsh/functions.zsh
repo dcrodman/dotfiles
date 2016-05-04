b35b13e78b7f2799459c1a636e79ebdd7a14ee46
Drew Rodman <arodman@pandora.com>
2016-05-03 17:37:52 -0700

    Remove redundant functions

--- zsh/functions.zsh
+++
@@ -1,9 +0,0 @@
-function shidden() {
-	defaults write com.apple.finder AppleShowAllFiles YES
-	killall Finder
-}
-
-function hhidden() {
-	defaults write com.apple.finder AppleShowAllFiles NO
-	killall Finder
-}
