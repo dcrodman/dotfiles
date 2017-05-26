9379c0599c6b83d149b69147c19bcfd25d9cd905
Drew Rodman <arodman@pandora.com>
2017-05-25 17:41:54 -0700

    Move some zsh configs into better places

--- zsh/alias.zsh
+++
@@ -1,7 +0,0 @@
-alias setgo='export GOPATH="$(pwd)"'
-
-alias shidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
-alias hhidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
-
-alias startmysql='mysql.server start'
-alias stopmysql='mysql.server stop'
--- zsh/env.zsh
+++
@@ -1,2 +0,0 @@
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
-export PATH=${PATH}:/opt/X11/include:/opt/X11/bin
