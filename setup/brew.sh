81aa5f0f25b8a2098a05a8dd3c8036b047a22a8b
Drew Rodman <dcrodman@gmail.com>
2017-11-18 19:47:07 -0800

    Updated things for a new setup

--- setup/brew.sh
+++ setup/brew.sh
@@ -5,39 +5,29 @@ if ! hash brew 2> /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew update
-brew upgrade
-
-brew install zsh 
-brew install git
+brew install zsh tmux
 brew install bash-completion
-brew install pyenv
-brew install ruby
-brew install go
-brew install wget
-brew install httpie
+brew install httpie wget
 brew install vim
-brew install ack
 brew install icdiff
 brew install mysql
 brew install aspell
-brew install tmux
+
+brew install git
+
+brew install pyenv
+brew install ruby
+brew install go delve
 
 # Needed for tmux config.
 brew install reattach-to-user-namespace
 
-brew install caskroom/cask/brew-cask
-brew install Caskroom/cask/xquartz
-brew cask install google-chrome
-brew cask install sublime-text3
-brew cask install iterm2
-
 # Remove outdated versions from the Cellar
 brew cleanup
 
-# Link .app files into /Applications
-brew linkapps
-
 echo "Note: Some things require manual installtion:"
 echo "Postgres.app (http://postgresapp.com/)"
-echo "Spacemacs (https://github.com/syl20bnr/spacemacs#install)"
+echo "Firefox"
+echo "SublimeText 3"
+echo "Visual Studio Code"
+echo "1Password"
