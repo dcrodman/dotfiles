5fb6c7e442e24375a6a73e873d346be7126b4906
Drew Rodman <dcrodman@gmail.com>
2021-03-02 21:19:50 -0800

    Updates to linux setup

--- setup/linux.sh
+++ setup/linux.sh
@@ -1,17 +1,14 @@
 sudo apt-get install zsh
 sudo apt-get install tmux
-sudo apt-get install xsel
 
 sudo apt-get install git-core
 sudo apt-get install vim
+sudo apt-get install ripgrep
 
 sudo apt-get install mysql-server
 sudo apt-get install postgresql
 
-sudo add-apt-repository ppa:gophers/archive
-sudo apt-get install golang-1.10-go
-
-sudo apt-get install python3.7
+sudo apt-get install python
 curl https://pyenv.run | bash
 
 chsh -s /bin/zsh
