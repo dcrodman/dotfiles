81819eaebced7c0cfea83b36ce29ba347e06cff5
Drew Rodman <dcrodman@gmail.com>
2021-12-27 21:53:17 -0500

    Update brew installs

--- setup/brew.sh
+++ setup/brew.sh
@@ -5,27 +5,17 @@ if ! hash brew 2> /dev/null; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-brew tap caskroom/fonts
-brew cask install font-hack-nerd-font
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
 
-brew install zsh \
-    # Environment & shell.
-    starship \
-    bash-completion \
-    tmux \
-    # (needed for tmux config)
-    reattach-to-user-namespace \
-    # Tools.
-    httpie \
-    wget \
-    knqyf263/pet/pet \
-    ripgrep \
-    tldr \
-    # Development stuff.
-    git \
-    vim \
-    pyenv \
-
+# Environment & shell.
+brew install zsh starship bash-completion tmux
+# (needed for tmux config)
+brew install reattach-to-user-namespace
+# Tools.
+brew install wget knqyf263/pet/pet ripgrep tldr
+# Development stuff.
+brew install git vim pyenv go
 
 # Remove outdated versions from the Cellar
 brew cleanup
