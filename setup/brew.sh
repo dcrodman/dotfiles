f217bc11923e242f2988442a8607ab9f6845fcaf
Drew Rodman <arodman@mux.com>
2022-04-19 13:15:56 -0400

    better brew script organization

--- setup/brew.sh
+++ setup/brew.sh
@@ -5,17 +5,26 @@ if ! hash brew 2> /dev/null; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-brew tap homebrew/cask-fonts
-brew install font-hack-nerd-font
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
 
-# Environment & shell.
-brew install zsh starship bash-completion tmux
-# (needed for tmux config)
-brew install reattach-to-user-namespace
-# Tools.
-brew install wget knqyf263/pet/pet ripgrep tldr nmap netcat
-# Development stuff.
-brew install git vim pyenv nvm go
+brew install zsh \
+    # Environment & shell.
+    starship \
+    bash-completion \
+    tmux \
+    # (needed for tmux config)
+    reattach-to-user-namespace \
+    # Tools.
+    wget \
+    knqyf263/pet/pet \
+    ripgrep \
+    # Development stuff.
+    git \
+    vim \
+    pyenv \
+    rbenv
+
 
 # Remove outdated versions from the Cellar
 brew cleanup
